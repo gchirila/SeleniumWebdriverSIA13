cf69507eae2a213c11187cecdd6837883d43bd4b
GChirila <GChirila@NBKR004549>
2022-03-21 16:19:28 +0200

    Add project files.

---
+++ SeleniumWebdriverSIA13/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumWebdriverSIA13")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumWebdriverSIA13")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f10d9ad-19df-4d1b-a1cf-73cfe85b2545")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumWebdriverSIA13/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace SeleniumWebdriverSIA13
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UserShouldLoginSuccessfully()
+        {
+            //open the browser
+            var driver = new ChromeDriver();
+            //maximize the page
+            driver.Manage().Window.Maximize();
+            //open the application url
+            driver.Navigate().GoToUrl("http://a.testaddressbook.com/");
+            //click sign in button from the menu
+            driver.FindElement(By.Id("sign-in")).Click();
+            //fill email
+            Thread.Sleep(2000);
+            var email = driver.FindElement(By.Id("session_email"));
+            email.SendKeys("test@test.test");
+            //fill password
+            var password = driver.FindElement(By.Name("session[password]"));
+            password.SendKeys("test");
+            //click login button
+            var loginButton = driver.FindElement(By.CssSelector("input[value='Sign in']"));
+            loginButton.Click();
+            Thread.Sleep(2000);
+            //verify/assert that the user email is the correct one
+            var loggedInUserEmail = driver.FindElement(By.XPath("//span[@data-test='current-user']"));
+            var userEmail = loggedInUserEmail.Text;
+            Assert.AreEqual("test@test.test", loggedInUserEmail.Text);
+            Assert.IsTrue(loggedInUserEmail.Text.Equals("test@test.test"));
+            //Close/Quit the browser
+            driver.Quit();
+        }
+    }
+}
